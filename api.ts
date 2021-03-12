83e01289760b6cb48ab6c3273ee7a8295faa4bf1
Doron <46745390+Dorona96@users.noreply.github.com>
2021-03-12 15:30:55 +0200

    Add files via upload

---
+++ api.ts
@@ -0,0 +1,115 @@
+import axios from "axios";
+import { APIRootPath } from "@fed-exam/config";
+
+export type Ticket = {
+  id: string;
+  title: string;
+  content: string;
+  creationTime: number;
+  userEmail: string;
+  labels?: string[];
+  answer?: string;
+};
+
+export type ApiClient = {
+  getTickets: (sortBy?: string, page?: number) => Promise<Ticket[]>;
+};
+
+axios
+  .get("/user", {
+    params: {
+      ID: 12345,
+    },
+  })
+  .then(function (response) {
+    console.log(response);
+  });
+
+
+
+
+
+export const createApiClient = (): ApiClient => {
+  return {
+    getTickets: (sortBy?: string, page?: number) => {
+      return axios
+        .get(APIRootPath, {
+          params: { page },
+        })
+        .then((res) => {
+          var unsortedTickets: Ticket[] = res.data.tempData;
+          sortedArray= res.data.tempData;
+          totalTicket = res.data.x;
+          if (sortBy) {
+            if (sortBy == "date") {
+              sortedArray = unsortedTickets.sort((a, b) => {
+                if (a.creationTime > b.creationTime) {
+                  return 1;
+                }
+                if (a.creationTime < b.creationTime) {
+                  return -1;
+                }
+                return 0;
+              }); //end sort by date
+              var curTP = sortedArray.slice((res.data.page - 1) * 20, res.data.page * 20);
+              return curTP;
+            } else if (sortBy == "email") {
+              sortedArray = unsortedTickets.sort((a, b) => {
+                if (a.userEmail > b.userEmail) {
+                  return 1;
+                }
+                if (a.userEmail < b.userEmail) {
+                  return -1;
+                }
+                return 0;
+              }); //end sort by email
+
+              var curTP = sortedArray.slice((res.data.page - 1) * 20, res.data.page * 20);
+              return curTP;
+            } else {
+              //sortBy=== title
+              sortedArray = unsortedTickets.sort((a, b) => {
+                if (a.title> b.title) {
+                  return 1;
+                }
+                if (a.title < b.title) {
+                  return -1;
+                }
+                return 0;
+              }); //end sort by title
+              var curTP = sortedArray.slice((res.data.page - 1) * 20, res.data.page * 20);
+              return curTP;
+            }
+          } // end if sortBy != null
+          return res.data.paginatedData;
+        }); 
+    }
+  }; 
+};
+
+
+export const upDate=(ticket:Ticket,answer:string)=>{
+  console.log("enterd update function");
+return  axios
+.patch(APIRootPath
+  ,
+  {
+        id: ticket.id,
+        title: ticket.title,
+        content: ticket.content,
+        userEmail: ticket.userEmail,
+        creationTime: ticket.creationTime,
+        labels:ticket.labels,
+        answer: answer
+
+  }
+)
+.then(res => console.log("submited answer"))
+.catch(err => console.error(err));
+}
+
+
+
+export var totalTicket: number;
+export var sortedArray: Ticket[];
+
